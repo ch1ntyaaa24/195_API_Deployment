7b7cdd76938279901b59fdf1e1d3c8a565e61d72
Chintya-aa <tyachintyachin@gmail.com>
2026-08-05 14:23:20 +0700

    data genre

---
+++ models/genre.js
@@ -0,0 +1,25 @@
+module.exports = (sequelize, DataTypes) => {
+    const Genre = sequelize.define("Genre", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+
+        nama: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+
+        deskripsi: {
+            type: DataTypes.TEXT,
+            allowNull: true
+        }
+
+    }, {
+        tableName: "genre",
+        timestamps: true
+    });
+
+    Genre.associate = (models) => {
