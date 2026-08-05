040fd29fc27192957cefc7c56ca8cfa160ee8efb
Chintya-aa <tyachintyachin@gmail.com>
2026-08-05 14:35:20 +0700

    data komik

---
+++ models/komik.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+    const Komik = sequelize.define("Komik", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+
+        judul: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+        sinopsis: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+
+        tahun_terbit: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+
+        penulis_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
